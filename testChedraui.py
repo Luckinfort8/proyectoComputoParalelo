d9bfdd854d0559f61f9eb876c19f481ad540c895
Luckinfort8 <krodriguezt2002@hotmail.com>
2023-11-05 15:42:18 -0600

    driver de walmart

--- testChedraui.py
+++ testChedraui.py
@@ -1,13 +1,15 @@
 from drivers.chedraui import chedraui_driver
+from drivers.walmart import walmart_driver
 
 
 def main():
-    info_total = chedraui_driver(('television', ['soporte'],
-                                  [('32 pulgadas', '32'),
-                                   ('55 pulgadas', '55'),
-                                   ('65 pulgadas', '65'),
-                                   ('75 pulgadas', '75'),
-                                   ]))
+
+    info_total = walmart_driver(('television', ['soporte'],
+                                    [('32 pulgadas', '32'),
+                                    ('55 pulgadas', '55'),
+                                    ('65 pulgadas', '65'),
+                                    ('75 pulgadas', '75'),
+                                    ]))
     for query, info in info_total:
         print(query)
         for item in info:
